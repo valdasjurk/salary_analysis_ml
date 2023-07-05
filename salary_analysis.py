ccd906151fd6627ab191a7078dea8b39ff3de380
Aidis <aidiss@gmail.com>
2023-07-05 19:07:13 +0300

    Parameter grid and refactoring

--- salary_analysis.py
+++ salary_analysis.py
@@ -1,12 +1,12 @@
 import pandas as pd
-from sklearn.compose import ColumnTransformer
-
+from sklearn.compose import ColumnTransformer, make_column_transformer
 # from sklearn.ensemble import RandomForestClassifier, RandomForestRegressor
 from sklearn.impute import SimpleImputer
 from sklearn.linear_model import LinearRegression
 from sklearn.model_selection import train_test_split
-from sklearn.pipeline import make_pipeline
+from sklearn.pipeline import Pipeline, make_pipeline
 from sklearn.preprocessing import OneHotEncoder, PolynomialFeatures
+from sklearn.model_selection import ParameterGrid
 
 DATA_PATH = "https://get.data.gov.lt/datasets/gov/lsd/darbo_uzmokestis/DarboUzmokestis2018/:format/csv"
 
@@ -17,11 +17,9 @@ def load_data():
     return data.head(5000)
 
 
-def create_model(X, y):
+def create_model():
     cat_features = ["lytis"]
-    cat_transformer = make_pipeline(
-        OneHotEncoder(handle_unknown="ignore"),
-    )
+    cat_transformer = make_pipeline(OneHotEncoder(handle_unknown="ignore"))
 
     num_features = ["profesija", "stazas", "darbo_laiko_dalis"]
     num_transformer = make_pipeline(SimpleImputer(strategy="most_frequent"))
@@ -32,16 +30,10 @@ def create_model(X, y):
             ("cat", cat_transformer, cat_features),
         ]
     )
-
     model = make_pipeline(
         preprocessor, PolynomialFeatures(degree=3), LinearRegression()
     )
-
-    tts = train_test_split(X, y, test_size=0.3)
-    X_train, X_test, y_train, y_test = tts
-
-    model.fit(X_train, y_train)
-    return model, model.score(X_test, y_test)
+    return model
 
 
 def predict_salary(
@@ -49,59 +41,52 @@ def predict_salary(
 ):
     """Sex = [M, F] , profession_name = gydytojas, experience in years, workload in percentage 0-100%"""
     profession_dict = {"administratorius": 334}
-    z = pd.DataFrame(
-        [
-            {
-                "lytis": sex,
-                "profesija": profession_dict[profession_name],
-                "stazas": experience,
-                "darbo_laiko_dalis": workload,
-            }
-        ]
-    )
+    data = {
+        "lytis": sex,
+        "profesija": profession_dict[profession_name],
+        "stazas": experience,
+        "darbo_laiko_dalis": workload,
+    }
+    z = pd.DataFrame([data])
     result = {"result": {"Yearly salary prediction, eur": round(model.predict(z)[0])}}
     print(result)
     return result
 
 
-def predict_salary_various_scenarios(
-    model, profession_name: str, experience_year=[1, 10]
-):
-    """Profession_name = gydytojas; experience_year = [1, 10] (egzample).
-    Function returns dictionary with yearly salary predictions of given profession and year interval for male and female
-    """
-    profession_dict = {"administratorius": 334}
-    result = {"result": {}}
-    for sex in ["M", "F"]:
-        for i in range(experience_year[0], experience_year[1]):
-            z = pd.DataFrame(
-                [
-                    {
-                        "lytis": sex,
-                        "profesija": profession_dict[profession_name],
-                        "stazas": i,
-                        "darbo_laiko_dalis": 100,
-                    }
-                ]
-            )
-            new_dict = {
-                f"Yearly salary prediction, eur [sex: {sex}, experience: {i}]": round(
-                    model.predict(z)[0]
-                )
-            }
-            result["result"].update(new_dict)
-    return result
+def create_testing_scenarios(experience_year=[1, 31]):
+    exp_year_start, exp_year_end = experience_year
+    param_grid = {
+        "lytis": ["F", "M"],
+        "stazas": range(exp_year_start, exp_year_end, 3),
+        "darbo_laiko_dalis": range(50, 101, 25),
+        "profesija": [334]
+    }
+    return pd.DataFrame(ParameterGrid(param_grid))
 
 
-if __name__ == "__main__":
-    data = load_data()
+def prepare_data(data):
     X, y = (
         data[["lytis", "profesija", "stazas", "darbo_laiko_dalis"]],
         data["dbu_metinis"],
     )
+    return train_test_split(X, y, test_size=0.3)
 
-    model, score = create_model(X, y)
+
+if __name__ == "__main__":
+    data = load_data()
+    X_train, X_test, y_train, y_test = prepare_data(data)
+    print("Train test splitted")
+    model = create_model()
+    print(model)
+    model.fit(X_train, y_train)
+    print("FITTED")
+    prediction = model.predict(X_test)
+    print("prediction")
+    score = model.score(X_test, y_test)
     print(score)
-    # predict_salary(modelis, "F", "gydytojas", 1, 100)
-    # scenarios = predict_salary_various_scenarios(model, "administratorius", experience_year=[1, 10])
-    # print(scenarios)
+    # predict_salary(model, "F", "gydytojas", 1, 100)
+    scenarios = create_testing_scenarios()
+    print(scenarios)
+    predictions = model.predict(scenarios)
+    r = scenarios.assign(predictions=predictions)
+    print(r)
