390b4936b3313c6bb47ad8b4b5697b25f56fec01
valdasjurk <valdas.jurk@gmail.com>
2023-07-10 17:44:24 +0300

    add best parameter finder function

--- salary_analysis.py
+++ salary_analysis.py
@@ -1,5 +1,6 @@
 import pandas as pd
 from sklearn.compose import ColumnTransformer, make_column_transformer
+
 # from sklearn.ensemble import RandomForestClassifier, RandomForestRegressor
 from sklearn.impute import SimpleImputer
 from sklearn.linear_model import LinearRegression
@@ -7,6 +8,7 @@ from sklearn.model_selection import train_test_split
 from sklearn.pipeline import Pipeline, make_pipeline
 from sklearn.preprocessing import OneHotEncoder, PolynomialFeatures
 from sklearn.model_selection import ParameterGrid
+from sklearn.model_selection import GridSearchCV
 
 DATA_PATH = "https://get.data.gov.lt/datasets/gov/lsd/darbo_uzmokestis/DarboUzmokestis2018/:format/csv"
 
@@ -59,7 +61,7 @@ def create_testing_scenarios(experience_year=[1, 31]):
         "lytis": ["F", "M"],
         "stazas": range(exp_year_start, exp_year_end, 3),
         "darbo_laiko_dalis": range(50, 101, 25),
-        "profesija": [334]
+        "profesija": [334],
     }
     return pd.DataFrame(ParameterGrid(param_grid))
 
@@ -70,6 +72,49 @@ def prepare_data(data):
         data["dbu_metinis"],
     )
     return train_test_split(X, y, test_size=0.3)
+
+
+def preprocese():
+    cat_features = ["lytis"]
+    cat_transformer = Pipeline(steps=[("ohe", OneHotEncoder(handle_unknown="ignore"))])
+
+    num_features = ["profesija", "stazas", "darbo_laiko_dalis"]
+    num_transformer = Pipeline(
+        steps=[("imputer", SimpleImputer(strategy="most_frequent"))]
+    )
+
+    preprocessor = ColumnTransformer(
+        transformers=[
+            ("num", num_transformer, num_features),
+            ("cat", cat_transformer, cat_features),
+        ]
+    )
+    return preprocessor
+
+
+def find_best_model_parameters(X_train, X_test, y_train, y_test):
+    preprocessed = preprocese()
+    pipeline = Pipeline(
+        [
+            ("preprocessor", preprocessed),
+            ("pol", PolynomialFeatures(degree=3)),
+            ("lin", LinearRegression()),
+        ]
+    )
+
+    parameters = {
+        "preprocessor__num__imputer__strategy": ["most_frequent", "mean"],
+        "pol__degree": [1, 2, 3, 4, 5],
+    }
+
+    grid_search = GridSearchCV(pipeline, parameters, n_jobs=-1, cv=3)
+    grid_search.fit(X_train, y_train)
+    grid_search.score(X_test, y_test)
+
+    x = pd.DataFrame(grid_search.cv_results_)
+    x = x.set_index("rank_test_score")
+    x = x.sort_index()
+    return x.loc[:, "param_pol__degree":"param_preprocessor__num__imputer__strategy"]
 
 
 if __name__ == "__main__":
@@ -84,9 +129,13 @@ if __name__ == "__main__":
     print("prediction")
     score = model.score(X_test, y_test)
     print(score)
-    # predict_salary(model, "F", "gydytojas", 1, 100)
+
     scenarios = create_testing_scenarios()
     print(scenarios)
     predictions = model.predict(scenarios)
     r = scenarios.assign(predictions=predictions)
     print(r)
+
+    best_parameters = find_best_model_parameters(X_train, X_test, y_train, y_test)
+    print("best parameters":)
+    print(best_parameters)
