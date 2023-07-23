ca30b804275301fd4fbcedeb45bc321724f8eac3
valdasjurk <valdas.jurk@gmail.com>
2023-07-23 22:32:05 +0300

    initial commit

---
+++ test_request.py
@@ -0,0 +1,15 @@
+import requests
+
+API = "http://localhost:8000/predict"
+
+sample_tip_request_json = {
+    "lytis": "M",
+    "profesija": 722,
+    "stazas": 10,
+    "darbo_laiko_dalis": 100,
+    "amzius": "30-39",
+}
+
+response = requests.post(API, json=sample_tip_request_json)
+data = response.json()
+print(data)
