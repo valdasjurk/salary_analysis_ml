34d2dea5c1c887275af7e2a33f5fd8a42a7d9aed
valdasjurk <valdas.jurk@gmail.com>
2023-07-23 22:31:50 +0300

    initial commit

---
+++ README.md
@@ -0,0 +1,9 @@
+# Machine learning WEB API with FastAPI
+
+```bash
+pip install -r requirements.txt
+```
+
+```bash
+python -m uvicorn app:app --reload
+```
---
+++ app.py
@@ -0,0 +1,28 @@
+import joblib
+from fastapi import FastAPI
+from pydantic import BaseModel
+import pandas as pd
+
+
+class Salary(BaseModel):
+    lytis: str
+    profesija: int
+    stazas: int
+    darbo_laiko_dalis: int
+    amzius: str
+
+
+app = FastAPI()
+
+
+@app.on_event("startup")
+async def load_model():
+    global model
+    model = joblib.load("model.joblib")
+
+
+@app.post("/predict")
+def predict(salary: Salary):
+    data = pd.DataFrame([dict(salary)])
+    prediction = model.predict(data)
+    return {"prediction": prediction.tolist()}
