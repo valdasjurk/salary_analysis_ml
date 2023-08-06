4c0af7bf3ff7c512dd7bc400b94867a0e5c32a30
valdasjurk <valdas.jurk@gmail.com>
2023-08-06 18:11:43 +0300

    move predict_salary func to separate file

--- salary_analysis.py
+++ salary_analysis.py
@@ -88,23 +88,6 @@ def create_lgbm_model() -> Pipeline:
     return model
 
 
-def predict_salary(
-    model, sex: str, profession_name: str, experience: float, workload: float
-) -> dict:
-    """Sex = [M, F] , profession_name = gydytojas, experience in years, workload in percentage 0-100%"""
-    profession_dict = {"administratorius": 334}
-    data = {
-        "lytis": sex,
-        "profesija": profession_dict[profession_name],
-        "stazas": experience,
-        "darbo_laiko_dalis": workload,
-    }
-    z = pd.DataFrame([data])
-    result = {"result": {"Yearly salary prediction, eur": round(model.predict(z)[0])}}
-    print(result)
-    return result
-
-
 def split_data_to_xy(data: pd.DataFrame) -> tuple[pd.DataFrame, pd.Series]:
     X, y = (
         data[XCOLS],
