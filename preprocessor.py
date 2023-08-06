89c27938884a4ba483c78f5ee157306a0db8df41
valdasjurk <valdas.jurk@gmail.com>
2023-08-06 21:58:33 +0300

    add custom transformer (not working!)

--- preprocessor.py
+++ preprocessor.py
@@ -2,14 +2,24 @@ from sklearn.compose import ColumnTransformer
 from sklearn.impute import SimpleImputer
 from sklearn.pipeline import Pipeline
 from sklearn.preprocessing import OneHotEncoder
+from custom_profession_name_transformer import ProfessionTransformer
 
 NUM_FEATURES = ["profesija", "stazas", "darbo_laiko_dalis"]
 CAT_FEATURES = ["lytis", "amzius"]
+CUSTOM_FEATURES = ["profesija"]
 
 
 def create_preprocessor() -> ColumnTransformer:
-    cat_transformer = Pipeline(steps=[("ohe", OneHotEncoder(handle_unknown="ignore"))])
-
+    cat_transformer = Pipeline(
+        steps=[
+            ("ohe", OneHotEncoder(handle_unknown="ignore")),
+        ]
+    )
+    # cust_transformer = Pipeline(
+    #     steps=[
+    #         ("prof_name", ProfessionTransformer()),
+    #     ]
+    # )
     num_transformer = Pipeline(
         steps=[("imputer", SimpleImputer(strategy="most_frequent"))]
     )
@@ -18,6 +28,7 @@ def create_preprocessor() -> ColumnTransformer:
         transformers=[
             ("num", num_transformer, NUM_FEATURES),
             ("cat", cat_transformer, CAT_FEATURES),
+            # ("cust", cust_transformer, CUSTOM_FEATURES),
         ]
     )
     return preprocessor
