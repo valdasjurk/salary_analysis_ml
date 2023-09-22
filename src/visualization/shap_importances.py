c600bbdeaa9fca8cb6c66c2f72b1fced5cd35a45
valdasjurk <valdas.jurk@gmail.com>
2023-09-22 17:36:37 +0300

    reduce column name parsing steps

--- src/visualization/shap_importances.py
+++ src/visualization/shap_importances.py
@@ -2,34 +2,25 @@ import shap
 import numpy as np
 import matplotlib.pyplot as plt
 
-MODEL_NAME = "rfr"  # RandomForestRegressor
-
 
 def parse_x_column_names(model):
-    preprocessor_steps = [
-        ["num", "imputer"],
-        ["cat", "ohe"],
-        ["cust", "ohe_cust"],
-    ]
-    col_names_list = np.array([])
-    for i in preprocessor_steps:
-        col_names = (
-            model.named_steps["preprocessor"]
-            .named_transformers_[i[0]][i[1]]
-            .get_feature_names_out()
-        )
-        col_names_list = np.append(col_names_list, col_names)
-    return col_names_list
+    column_names = (
+        model.named_steps["preprocessor"]
+        .named_steps["col_tran"]
+        .get_feature_names_out()
+    )
+    return column_names
 
 
 def plot_shap_importances(model, X_train, y_train, show_or_save="show"):
     # explain the model's predictions using SHAP
-    column_names = parse_x_column_names(model)
     model.fit(X_train, y_train)
+    model_name = model.steps[-1][0]
+    column_names = parse_x_column_names(model)
     model_masker = model[:-1].transform(X_train).toarray()
 
     explainer = shap.Explainer(
-        model.named_steps[MODEL_NAME],
+        model.named_steps[model_name],
         masker=model_masker,
         feature_names=column_names,
     )
